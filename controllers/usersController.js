fd3ef9219ea10e45538f983deb1702f33058c65b
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-04-23 11:28:14 -0600

    added files from previous classwork

---
+++ controllers/usersController.js
@@ -0,0 +1,165 @@
+"use strict";
+
+const passport = require("passport");
+
+const User = require("../models/user"),
+    getUserParams = body => {
+        return {
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email,
+            passwork: body.password,
+            zipCode: body.zipCode
+        }
+    }
+
+module.exports = {
+    login: (req, res) => {
+        res.render("users/login");
+    },
+    index: (req, res, next) => {
+        User.find()
+            .then(users => {
+                res.locals.users = users;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching subscrber data: ${error.message} `);
+                next(error);
+            })
+    },
+    indexView: (req, res) => {
+        res.render("users/index");
+    },
+    new: (req, res) => {
+        res.render("users/new");
+    },
+    create: (req, res, next) => {
+        if(req.skip) return next();
+
+        let userParams = getUserParams(req.body);
+        let newUser = new User(userParams);
+
+        User.register(newUser, req.body.password, (error, user) => {
+            if(user) {
+                req.flash("success", "User account successfully created!");
+                res.locals.redirect = "/users";
+                next();
+            }
+            else {
+                req.flash("error", `Failed to create user account: ${error.message}`);
+                res.locals.redirect = "/users/new";
+                next();
+            }
+        });
+    },
+    validate: (req, res, next) => {
+        req.sanitizeBody("email").normalizeEmail({
+            all_lowercase: true
+        }).trim();
+
+        req.check("email", "Email is not valid!").isEmail();
+        req.check("zipCode", "Zip Code is not valid!").notEmpty().isInt().isLength({
+            min: 5,
+            max: 5
+        });
+        req.check("password", "Password cannot be empty!").notEmpty();
+
+        req.getValidationResult().then((error) => {
+            if(!error.isEmpty()) {
+                let messages = error.array().map(e => e.msg);
+                req.flash("error", messages.join(" and "));
+                req.skip = true;
+                res.locals.redirect = "/users/new";
+                next();
+            }
+            else {
+                next();
+            }
+        });
+    },
+    authenticate: passport.authenticate("local", {
+        failureRedirect: "/users/login",
+        failureFlash: "Login failed! Check your email or password!",
+        successRedirect: "/",
+        successFlash: "Logged in!"
+    }),
+    logout: (req, res, next) => {
+        req.logout();
+        req.flash("success", "Successfully logged out!");
+        res. locals.redirect = "/";
+        next();
+    },
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath != undefined) res.redirect(redirectPath);
+        else next();
+    },
+    show: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+            .then(user => {
+                res.locals.user = user;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching user by ID: ${error.message}`);
+                next(error);
+            })
+    },
+    showView: (req, res) => {
+        res.render("users/show")
+    },
+    edit: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+            .then(user => {
+                res.render("users/edit", { user: user });
+            })
+            .catch(error => {
+                console.log(`Error loading user by ID: ${error.message}`);
+                next(error);
+            })
+    },
+    update: (req, res, next) => {
+        if(req.skip) return next();
+
+        let userId = req.params.id;
+        // let updatedUser = new User ({
+        //     firstName: req.body.firstName,
+        //     lastName: req.body.lastName,
+        //     email: req.body.email,
+        //     zipCode: req.body.zipCode,
+        //     password: req.body.password
+        // });
+        var updatedUser = {};
+        updatedUser.firstName = req.body.firstName;
+        updatedUser.lastName =  req.body.lastName;
+        updatedUser.email = req.body.email;
+        updatedUser.zipCode = req.body.zipCode;
+        updatedUser.password = req.body.password;
+        
+        User.findByIdAndUpdate(userId, updatedUser)
+        .then(user => {
+            res.locals.user = user;
+            res.locals.redirect = `/users/${user._id}`;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error loading user by ID: ${error.message}`);
+            next(error);
+        })
+    }, 
+    delete: (req, res, next) => {
+        let userId = req.params.id;
+        User.findByIdAndRemove(userId)
+        .then(() => {
+            res.locals.redirect = "/users";
+            next();
+        })
+        .catch(error => {
+            console.log(`Error loading user by ID: ${error.message}`);
+            next(error);
+        })
+    }
+}
---
+++ main.js
@@ -0,0 +1,111 @@
+const { authenticate } = require("./models/user");
+
+const express = require("express"), 
+    app = express(),
+    router = express.Router(),
+    layouts = require("express-ejs-layouts"), 
+    mongoose = require("mongoose"),
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    subscribersController = require("./controllers/subscribersController"),
+    usersController = require("./controllers/usersController"),
+    coursesController = require("./controllers/coursesController"),
+    methodOverride = require("method-override"),
+    passport = require("passport"),
+    cookieParser = require("cookie-parser"),
+    expressSession = require("express-session"),
+    expressValidator = require("express-validator"),
+    connectFlash = require("connect-flash"),
+    User = require("./models/user");
+
+mongoose.connect(
+    "mongodb://localhost:27017/confetti_cuisine", 
+{ useNewUrlParser: true },
+{ useUnifiedTopology: true }
+);
+
+mongoose.set("useCreateIndex", true);
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+router.use(methodOverride("_method", {
+    methods: ["POST", "GET", ]
+}));
+
+router.use(layouts);
+router.use(express.static("public"));
+router.use(expressValidator());
+router.use(express.json());
+
+router.use(cookieParser("my_passcode"));
+router.use(expressSession({
+    secret: "my_passcode",
+    cookie: {
+        maxAge: 360000
+    },
+    resave: false,
+    saveUninitialized: false
+}));
+
+router.use(connectFlash());
+
+router.use(passport.initialize());
+router.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+router.use((req, res, next) => {
+    res.locals.loggedIn = req.isAuthenticated();
+    res.locals.currentUser = req.user;
+    res.locals.flashMessages = req.flash();
+    next();
+});
+
+router.get("/", homeController.index);
+
+// subscriber routes
+router.get("/subscribers", subscribersController.index, subscribersController.indexView);
+router.get("/subscribers/new", subscribersController.new);
+router.post("/subscribers/create", subscribersController.create, subscribersController.redirectView);
+router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
+router.get("/subscribers/:id/edit", subscribersController.edit);
+router.put("/subscribers/:id/update", subscribersController.update, subscribersController.redirectView);
+router.delete("/subscribers/:id/delete", subscribersController.delete, subscribersController.redirectView);
+
+// user routes
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/users/create", usersController.validate, usersController.create, usersController.redirectView);
+router.get("/users/login", usersController.login);
+router.post("/users/login", usersController.authenticate);
+router.get("/users/logout", usersController.logout, usersController.redirectView);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.validate, usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+
+// course routes
+router.get("/courses", coursesController.index, coursesController.indexView);
+router.get("/courses/new", coursesController.new);
+router.post("/courses/create", coursesController.create, coursesController.redirectView);
+router.get("/courses/:id", coursesController.show, coursesController.showView);
+router.get("/courses/:id/edit", coursesController.edit);
+router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
+
+// error handling
+router.use(errorController.pageNotFoundError);
+router.use(errorController.internalServerError);
+
+app.use("/", router);
+
+app.listen(app.get("port"), () => {
+    console.log(`Server is running on port: ${app.get("port")}`)
+});

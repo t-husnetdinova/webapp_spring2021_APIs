1d31f0e0bfafec3fe66ada2df495a066fd54908a
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-04-24 19:31:21 -0600

    added confettiCuisine.js modal logic

---
+++ public/js/confettiCuisine.js
@@ -0,0 +1,49 @@
+// where we add data to populate client side template
+// it is here because this is front end, unlike other backend js files
+
+$(document).ready(() => {
+    $('#modal-button').click(() => {
+        $('.modal-body').html("");
+        $.get(`/api/courses`, (results = {}) => {
+            let data = results.data;
+            if (!data || !data.courses) return;
+            console.log('data', data)
+            data.courses.forEach(course => {
+                $('.modal-body').append(
+                    `<div>
+                        <span class="course-title">${course.title}</span>
+                        <span class="course-cost">$${course.cost}</span>
+                        <button class="${course.joined ? "joined-button" : "join-button"} btn btn-info btn-sm" data-id="${course._id}">
+                            ${course.joined ? "Joined" : "Join"}
+                        </button>
+                        <div class="course-description">
+                            ${course.description}
+                        </div>
+                    </div>`
+                );
+            });
+        }).then(() => {
+            addJoinButtonListener();
+        });
+    });
+});
+
+let addJoinButtonListener = () => {
+    $('.join-button').click(event => {
+        let $button = $(event.target),
+            courseId = $button.data("id");
+        console.log(`/api/courses/${courseId}/join`)
+        $.get(`/api/courses/${courseId}/join`, (results = {}) => {
+            let data = results.data;
+            if (data && data.success) {
+                $button
+                    .text("Joined")
+                    .addClass("joined-button")
+                    .removeClass("join-button");
+            } else {
+                $button.text("Try again")
+            }
+        });
+
+    });
+};

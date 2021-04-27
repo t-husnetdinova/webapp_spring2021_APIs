7b6108d6b278744f28ea98ef9ff5c253e3366831
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-04-27 10:03:52 -0600

    added mongodb_uri param

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const express = require("express"),
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
+mongoose.connect(process.env.MONGODB_URI ||
     "mongodb://localhost:27017/confetti_cuisine", 
   { useNewUrlParser: true }
 );
